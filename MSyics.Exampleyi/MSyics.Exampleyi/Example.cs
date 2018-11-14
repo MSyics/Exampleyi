f39c9d0cea155c966e66a6ea310289599839869b
msyics <msyics@gmail.com>
2018-11-14 17:34:32 +0900

    initial commit

---
+++ MSyics.Exampleyi/MSyics.Exampleyi/Example.cs
@@ -0,0 +1,11 @@
+﻿using MSyics.Traceyi;
+
+namespace MSyics.Exampleyi
+{
+    internal abstract class Example
+    {
+        protected Tracer Tracer { get; } = Traceable.Get();
+        public abstract string Name { get; }
+        public abstract void Test();
+    }
+}
---
+++ MSyics.Exampleyi/MSyics.Exampleyi/ExampleEntry.cs
@@ -0,0 +1,38 @@
+﻿using MSyics.Traceyi;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSyics.Exampleyi
+{
+    abstract class ExampleEntry
+    {
+        public ExampleEntry()
+        {
+            Traceable.Add(TraceConfig);
+        }
+
+        virtual protected string TraceConfig => "Traceyi.json";
+        protected Tracer Tracer { get; } = Traceable.Get();
+        private List<Example> Examples { get; } = new List<Example>();
+
+        public ExampleEntry Add<T>() where T : Example, new()
+        {
+            Examples.Add(new T());
+            return this;
+        }
+
+        public void Test()
+        {
+            foreach (var item in Examples)
+            {
+                using (Tracer.Scope(item.Name))
+                {
+                    item.Test();
+                }
+            }
+            Traceable.Shutdown();
+        }
+    }
+}
