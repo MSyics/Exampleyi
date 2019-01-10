d93279a1888ef4bce2ddb2a8ab36363e653b65b5
msyics <msyics@gmail.com>
2019-01-10 17:16:46 +0900

    v2.0.0

---
+++ MSyics.Exampleyi/MSyics.Exampleyi/ExampleAggregator.cs
@@ -0,0 +1,49 @@
+﻿using MSyics.Traceyi;
+using System.Collections.Generic;
+
+/// <summary>
+/// 登録した実例を実行します。
+/// </summary>
+public sealed class ExampleAggregator
+{
+    /// <summary>
+    /// トレーサーオブジェクトを取得します。
+    /// </summary>
+    private Tracer Tracer { get; } = Traceable.Get();
+
+    /// <summary>
+    /// Example オブジェクトの一覧を取得します。
+    /// </summary>
+    private List<Example> Examples { get; } = new List<Example>();
+
+    /// <summary>
+    /// Example オブジェクトを登録します。
+    /// </summary>
+    /// <typeparam name="T">登録する Example オブジェクトの型</typeparam>
+    public ExampleAggregator Add<T>() where T : Example, new()
+    {
+        Examples.Add(new T());
+        return this;
+    }
+
+    /// <summary>
+    /// 実例を示します。
+    /// </summary>
+    public void Show()
+    {
+        try
+        {
+            using (Tracer.Scope())
+            {
+                foreach (var item in Examples)
+                {
+                    item.Show();
+                }
+            }
+        }
+        finally
+        {
+            Traceable.Shutdown();
+        }
+    }
+}
